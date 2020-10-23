5ad862ebdf4dc299db6711ebc45940c4f26b77f8
Tironas <ywukas@gmail.com>
2020-10-23 21:23:27 +0100

    Added mouse click movement

---
+++ Assets/Scripts/MovingScript.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovingScript : MonoBehaviour
+{
+    [SerializeField]
+    [Range(2,12)]
+    private float speed = 4;
+
+    private Vector3 targetPosition;
+
+    private bool isMoving = false;
+
+    // Start is called before the first frame update
+    void Start(){
+        
+    }
+
+    // Update is called once per frame
+    void Update(){
+        if (Input.GetMouseButton(0)){
+            SetTargetPosition();
+        }
+
+        if (isMoving){
+            Move();
+        }
+    }
+
+    void SetTargetPosition(){
+        targetPosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        targetPosition.z = transform.position.z;
+        
+        isMoving = true;
+    }
+
+    void Move(){
+        transform.rotation = Quaternion.LookRotation(Vector3.forward, targetPosition);
+        transform.position = Vector3.MoveTowards(transform.position, targetPosition, speed * Time.deltaTime);
+        if (transform.position == targetPosition){
+            isMoving = false;
+        }
+    }
+}
